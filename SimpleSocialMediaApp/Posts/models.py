e0af23b5caba0a4fbed2f5241e860b711170f98b
ADEYEYE TIMOTHY <adeyeyetimothy33@gmail.com>
2019-08-21 07:27:08 +0100

    Created Group and Post Apps and coded their models.py files

---
+++ SimpleSocialMediaApp/Posts/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.urls import reverse
+from django.conf import settings
+
+import misaka
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+from Groups.models import Group
+# Create your models here.
+# POSTS MODELS.PY FILE
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    created_at = models.DateTimeField(auto_now=True)
+    message = models.TextField()
+    message_html = models.TextField(editable=False)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, null=True, blank=True, related_name='posts')
+
+    def __str__(self):
+        return self.message
+
+    def save(self, *args, **kwargs):
+        self.message_html = misaka.html(self.message)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('posts:single', kwargs={'username':self.user.username, 'pk':self.pk})
+
+    class Meta:
+        ordering = [-'created_at']
+        constraints = [
+            models.UniqueConstraint(fields=['user', 'message'], name='a message uniquely linked to a user')
+        ]
