bd7aad15e1d6f9353c2fafdf3848d39bdc83ce38
ADEYEYE TIMOTHY <adeyeyetimothy33@gmail.com>
2019-08-24 19:16:38 +0100

    coded the template files of Groups app and the views.py file

--- SimpleSocialMediaApp/Groups/views.py
+++ SimpleSocialMediaApp/Groups/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.urls import reverse
+from django.views.generic import (CreateView,ListView,DetailView)
+from Groups.models import Group
 
+# GROUPS VIEWS.PY FILE
 # Create your views here.
+
+class CreateGroup(LoginRequiredMixin,CreateView):
+    fields = ('name', 'description')
+    model = Group
+
+class GroupDetail(DetailView):
+    model = Group
+    # context_object_name = 'group' DEFAULT >> lowercase of model name
+
+class GroupList(ListView):
+    model = Group
